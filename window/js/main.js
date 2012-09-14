faa738fd32f5dde25a3e242089ea908426fcaae9
guodayang <446741501@qq.com>
2012-09-14 16:16:01 +0800

    ~

--- window/js/main.js
+++ window/js/main.js
@@ -4,16 +4,21 @@ var url;
 var isBase=false;
 var transparent=false;
 var isDefault=false;
+/*
+ * å‡½æ•°åŠŸèƒ½ï¼šä»hrefè·å¾—å‚æ•°
+ * sArgName : arg1, arg2
+ * return :   the value of arg. d, re
+ */
 function getArg(sArgName)
 {
    var sHref = decodeURI(window.document.location.href);
    var args   = sHref.split("?");
    var retval = "";
 
-   if(args[0] == sHref) /* ²ÎÊıÎª¿Õ */
+   if(args[0] == sHref) /* å‚æ•°ä¸ºç©º */
    {
       return retval;
-      /* ÎŞĞè×öÈÎºÎ´¦Àí */
+      /* æ— éœ€åšä»»ä½•å¤„ç† */
    }
    var str = args[1];
    args = str.split("&");
@@ -30,28 +35,8 @@ function getArg(sArgName)
 document.onselectstart=function(){
 	return false;
 }
-var showDevHandler=function(){
-	if(!browserWindow){
-		webtop.showDev();
-	}
-	else{
-		webtop.showDev(browserWindow);
-	}
-
-}
-devBtn.onclick=function(){
-	showDevHandler();
-}
 closeBtn.onclick=function(){
 	closeHandler();
-}
-function switchMode(){
-	transparent=!transparent;
-	newPage(url,transparent,isBase);
-	isDefault=false;
-}
-transparentBtn.onclick=function(){
-	switchMode();
 }
 miniBtn.onclick=function(){
 	webtop.mini();
@@ -70,14 +55,14 @@ var sizeHandler=function(e){
 	var w=e.detail.width,h=e.detail.height;
 	container.style.height=h-12+'px';
 	if(browserWindow){
-		webtop.setSize(w-30, h-85, browserWindow);
+		webtop.setSize(w-30, h-55, browserWindow);
 	}
 	width=w;
 	height=h;
 }
 var moveHandler=function(e){
 	if(browserWindow){
-		webtop.move(e.detail.x+15,e.detail.y+70,browserWindow);
+		webtop.move(e.detail.x+15,e.detail.y+40,browserWindow);
 	}
 }
 var closeHandler=function(){
@@ -88,10 +73,9 @@ var focusHandler=function(){
 	if(browserWindow){
 		webtop.bringToTop(browserWindow);
 		webtop.restore(browserWindow);
-		//webtop.focus(browserWindow);
 	}
 }
-var activeHandler=function(){
+var activeHandler=function(x,y){
 	webtop.focus(browserWindow);
 }
 var dropHandler=function(e){
@@ -100,7 +84,6 @@ var dropHandler=function(e){
 	for(var i=1;i<fileList.length;++i){
 		webtop.browse(fileList[i]);
 	}
-	console.log(e.detail);
 }
 webtop.loadUrlIn=function(_url){
 	if(!browserWindow){
@@ -121,87 +104,34 @@ function newPage(url,transparent,isBase){
 		browserWindow=webtop.createWindow(url,WS_EX_TOOLWINDOW,transparent,"var parentWindow="+handler);
 	}
 	var pos=webtop.getPos();
-	webtop.move(pos.x+15,pos.y+70,browserWindow);
-	webtop.setSize(width-30, height-85, browserWindow);
+	webtop.move(pos.x+15,pos.y+40,browserWindow);
+	webtop.setSize(width-30, height-55, browserWindow);
 }
 var $=function(id){
 	return document.getElementById(id);
 }
-$('lst-ib').onkeyup=function(e){
-	if(e.keyCode==13){
-		/*if(isDefault){
-			isBase=false;
-			switchMode();
-			isDefault=false;
-		}*/
-		webtop.loadUrlIn($('lst-ib').value);
-		webtop.focus(browserWindow);
-		//newPage($('lst-ib').value);
-	}
-}
-makeBtn.onclick=function(){
-	var path='';
-	var index1=url.lastIndexOf('\\');
-	var index=url.lastIndexOf('.');
-	if(index1==-1){
-		index1=url.lastIndexOf('/');
-	}
-	if(index<index1){
-		index=url.length;
-	}
-	var s='[BASE]\n';
-	if(url.indexOf('http')==-1&&url.indexOf(':')!=-1){
-		s+='url='+url.substring(index1+1);
-		path=url.substr(0,index+1)+'app';
-	}
-	else{
-		if(url.indexOf(":")==-1){
-			s+='url=http://'+url;
-		}
-		else{
-			s+='url='+url;
-		}
-		path=url.substring(index1+1,index)+'.app';
-		path.replace(/\//g,'');
-		path=webtop.getSaveName( path);
-	}
-	s+='\nwidth=800\nheight=600\nenableDrag=1';
-	webtop.writeFile(path,s);
-}
+var href=location.href;
+var url=getArg('url');
+var param=getArg('param');
 var readyHandler=function(){
-	webtop.move(0,0);
-	webtop.max();
-	setTimeout("webtop.toImage('screen.png');",1000);
-	var href=location.href;
-	var param=href.substr(href.indexOf('=')+1);//getArg('param');
-	if(param){
-		webtop.loadUrlIn(param)
+	if(url){
+		if(param){
+			url+='?param='+param;
+		}
+		webtop.loadUrlIn(url)
 	}
-	/*else{
-		newPage('http://www.alloyteam.com');
-		//isBase=true;
-		url='http://www.alloyteam.com';
-		//transparent=true;
-		isDefault=true;
-	}*/
-	console.log('ready');
-}
-var refreshHandler=function(){
-	if(browserWindow){
-		webtop.reloadIgnoreCache(browserWindow);
+	webtop.move(getArg('x'),getArg('y'));
+	if(getArg('max')=='1'){
+		webtop.max();
 	}
 	else{
-		webtop.reloadIgnoreCache();
+		webtop.setSize(getArg('width'),getArg('height'));
 	}
 }
 addEventListener("webtopReady",readyHandler);
 addEventListener("webtopWindowResize",sizeHandler);
-addEventListener("webtopWindowMove",moveHandler);7
+addEventListener("webtopWindowMove",moveHandler);
 addEventListener("webtopDragDrop",dropHandler);
 addEventListener("webtopWindowActive",activeHandler);
 addEventListener("webtopWindowFocus",focusHandler);
-addEventListener("webtopRefresh",refreshHandler);
-addEventListener("webtopShowDev",showDevHandler);
-refreshBtn.onclick=function(){
-	refreshHandler()
-}
+title.innerHTML=getArg('name');
