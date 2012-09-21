9acd4139e4704246c0cc16281d615f74eb618edf
guodayang <446741501@qq.com>
2012-09-21 14:27:29 +0800

    ~

--- Release/ip.js
+++ Release/ip.js
@@ -1,7 +1,7 @@
 ﻿var node=document.createElement('div');
 node.style.cssText='position:absolute;top:200px;width:729px;font-size:12px;left:50px;color:red;-webkit-marquee:left 2px infinite 20ms scroll;overflow:-webkit-marquee;';
-node.innerHTML='官方群：257960168，欢迎交流讨论';
+node.innerHTML='官方群：257960168，欢迎交流讨论。2012.09.20：修复几个bug';
 wrapper.appendChild(node);
 var o=document.querySelector(".info");
 var link=o.getElementsByTagName('a')[1];
-link.href="http://download.alloyteam.com/webtop.zip?t=20120914001";
+link.href="http://download.alloyteam.com/webtop.zip?t=20120920001";
